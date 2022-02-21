8315eb28d2faa81cae02f0c9245c05b6d2aac653
kinza <bsef19a515@pucit.edu.pk>
2022-02-21 14:50:54 +0500

    Updates

---
+++ precache-manifest.69a01ae741962f9fd55c91663ff77312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7a36472fd19ae9b5f1296e653968d2",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "796919d41de6175fad3b",
+    "url": "/practice/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/practice/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/practice/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "796919d41de6175fad3b",
+    "url": "/practice/static/js/main.de699cfb.chunk.js"
+  },
+  {
+    "revision": "2d7aa67ed8043b7a0c5c",
+    "url": "/practice/static/js/runtime~main.2db8077a.js"
+  }
+]);
